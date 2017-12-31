092006bfe39f0bd4fad6ae06cf6a7524cf627ec2
jiikko <n905i.1214@gmail.com>
2017-12-31 12:52:37 +0900

    テストがグリーンになるコード

--- lib/sugoi_log_watcher/aggregater.rb
+++ lib/sugoi_log_watcher/aggregater.rb
@@ -1,11 +1,26 @@
 module SugoiLogWatcher
   class Aggregater
+    def initialize
+      @buffer = []
+      @compited = []
+    end
+
     def add(line)
-      puts line
+      @buffer << line
     end
 
     def complated
+      @compited
       [1]
+    end
+
+    def buffer
+      @buffer
+    end
+
+    def aggregate
+      # TODO
+      # ここで
     end
   end
 end
